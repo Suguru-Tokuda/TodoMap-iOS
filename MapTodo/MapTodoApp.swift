1f3303da9afa6e1f3cbddd8bd40c11acf4a058d5
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-08-05 18:58:30 -0500

    Initial Commit

---
+++ MapTodo/MapTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTodoApp.swift
+//  MapTodo
+//
+//  Created by Suguru on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MapTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
