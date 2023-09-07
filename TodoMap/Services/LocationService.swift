6228dc8248477beb59f73e091966f934a2bfd34a
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-09-07 18:59:03 -0500

    Added .gitignore, new files.

---
+++ TodoMap/Services/LocationService.swift
@@ -0,0 +1,62 @@
+//
+//  LocationService.swift
+//  TodoMap
+//
+//  Created by Suguru on 9/2/23.
+//
+
+import MapKit
+
+enum MapDetails {
+    static let startingLocation = CLLocationCoordinate2D(latitude: 37.33233141, longitude: -122.0312186)
+    static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+}
+
+class LocationService: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationService()
+    var locationManager: CLLocationManager?
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion(center: MapDetails.startingLocation, span: MapDetails.defaultSpan)
+    
+    func checkIfLocationServicesIsEnabled() async {
+        if CLLocationManager.locationServicesEnabled() {
+            self.locationManager = CLLocationManager()
+            self.locationManager!.delegate = self
+            self.updateCurrentLocation()
+        } else {
+            print("Show an alert letting them know this is off and to go turn it on")
+        }
+    }
+    
+    public func updateCurrentLocation() {
+        guard let locationManager = locationManager else { return }
+        if let location = locationManager.location {
+            mapRegion = MKCoordinateRegion(center: location.coordinate, span: MapDetails.defaultSpan)
+        }
+    }
+    
+    public func checkLocationAuthorization() {
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                print("Your location is restricted likely due to parental controls.")
+            case .denied:
+                print("You have denied this app location permission. Go into settings to change it.")
+            case .authorizedAlways, .authorizedWhenInUse:
+            updateCurrentLocation()
+            @unknown default:
+                break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        mapRegion.center.latitude = (manager.location?.coordinate.latitude)!
+        mapRegion.center.longitude = (manager.location?.coordinate.longitude)!
+    }
+}
