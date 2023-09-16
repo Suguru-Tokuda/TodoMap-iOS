3e95f05ef6eebca868681e9845fad570713ce631
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-09-15 23:49:25 -0500

    Added subscriptions.

--- TodoMap/Core/TodoMap/ViewModels/TodoMapViewModel.swift
+++ TodoMap/Core/TodoMap/ViewModels/TodoMapViewModel.swift
@@ -6,21 +6,25 @@
 //
 
 import MapKit
+import Combine
 
 class TodoMapViewModel: ObservableObject {
     @Published var mapRegion: MKCoordinateRegion?
-    @Published var annotations: [MKAnnotation] = []
+    @Published var annotations: [MKPointAnnotation] = []
+    @Published var locationSelectionSheeetPresented: Bool = false
+    @Published var location: ReverseGeocodeModel?
+    var presentSheetCancellable: Cancellable?
     
     init() {
-        if let region = LocationService.shared.mapRegion {
+        if let region = LocationService.shared.center {
             self.mapRegion = region
         }
         addSubscription()
     }
     
-    func addSubscription() {
+    private func addSubscription() {
         Task {
-            for await value in LocationService.shared.$mapRegion.values {
+            for await value in LocationService.shared.$center.values {
                 await MainActor.run(body: {
                     if let value = value {
                         self.mapRegion = value
@@ -28,11 +32,41 @@ class TodoMapViewModel: ObservableObject {
                 })
             }
         }
+        
+        Task {
+            presentSheetCancellable = self.$locationSelectionSheeetPresented
+                .receive(on: DispatchQueue.main)
+                .sink { value in
+                    print(value)
+                    if !value { self.annotations = [] }
+                    print(self.annotations.count)
+                }
+        }
     }
     
     func checkIfLocationServicesIsEnabled() {
         Task {
             await LocationService.shared.checkIfLocationServicesIsEnabled()
         }
+    }
+    
+    func getLocationDetails(coordinate: CLLocationCoordinate2D) async throws {
+        do {
+            if let location = try await MapsService.shared.getLocation(latitude: coordinate.latitude, longitude: coordinate.longitude) {
+                DispatchQueue.main.async {
+                    self.locationSelectionSheeetPresented = true
+                    self.location = location
+                }
+            }
+        } catch {
+            throw error
+        }
+    }
+    
+    func addAnnotation(annotation: MKPointAnnotation, reset: Bool = false) {
+        if reset { self.annotations = [annotation] }
+        else { self.annotations.append(annotation) }
+        
+        print(self.annotations.count)
     }
 }
