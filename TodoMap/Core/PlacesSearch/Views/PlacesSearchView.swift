702a82f498b301066a475a40be81d8a0f5788b1b
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-09-15 23:51:42 -0500

    Modified the view.

--- TodoMap/Core/PlacesSearch/Views/PlacesSearchView.swift
+++ TodoMap/Core/PlacesSearch/Views/PlacesSearchView.swift
@@ -9,25 +9,47 @@ import SwiftUI
 
 struct PlacesSearchView: View {
     @StateObject var vm = PlaceSearchViewModel()
+    var handleBackBtnTapped: (() -> ())?
+    @State var textFieldFocused: Bool = false
     
     var body: some View {
         ZStack {
-            Color.theme.background
-                .ignoresSafeArea()
-            VStack {
-                PlacesSearchBarView(text: $vm.searchText, isLoading: $vm.isLoading) {
-                    vm.resetSearch()
+            TodoMapView()
+                .background(ignoresSafeAreaEdges: .bottom)
+                .onTapGesture {
+                    UIApplication.shared.endEditing()
                 }
+                .opacity((!textFieldFocused && vm.predictions.count == 0 && vm.nearbySearchResults.count == 0) ? 1 : 0)
+            
+            if !(!textFieldFocused && vm.predictions.count == 0 && vm.nearbySearchResults.count == 0) {
+                Color.theme.background
+                    .ignoresSafeArea()
+            }
+                
+            VStack {
+                PlacesSearchBarView(
+                    text: $vm.searchText,
+                    isLoading: $vm.isLoading,
+                    handleCancelBtnTapped: {
+                        vm.resetSearch()
+                        resetSearch()
+                    },
+                    handleBackBtnTapped: {
+                        resetSearch()
+                        handleBackBtnTapped?()
+                    }) { focused in
+                        withAnimation(.easeIn) {
+                            textFieldFocused = focused
+                        }
+                    }
                 if !vm.predictions.isEmpty || !vm.nearbySearchResults.isEmpty {
                     PlaceSearchListView(
                         predictions: vm.predictions,
                         nearbySearchResults: vm.nearbySearchResults
                     )
                     .environmentObject(vm)
-                    Spacer()
-                } else {
-                    Spacer()
                 }
+                Spacer()
             }
             .padding(.horizontal, 10)
         }
@@ -39,9 +61,13 @@ struct PlacesSearchView: View {
     }
 }
     
-    extension PlacesSearchView {
-        
+extension PlacesSearchView {
+    private func resetSearch() {
+        withAnimation(.easeIn) {
+            textFieldFocused = false
+        }
     }
+}
 
 struct PlacesSearchView_Previews: PreviewProvider {
     static var previews: some View {
