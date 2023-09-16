e640454b351f69bc5939fb45d609663bb66eb0f7
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-09-15 23:53:10 -0500

    Added getLocation

--- TodoMap/Services/MapsService.swift
+++ TodoMap/Services/MapsService.swift
@@ -9,11 +9,12 @@ import MapKit
 
 class MapsService {
     static let shared = MapsService()
-    static let googleMapsBaseURL = "\(Constants.googleAPIBaseURL)maps/api/place/"
+    let googleMapsBaseURL = "\(Constants.googleAPIBaseURL)maps/api/place/"
+    let googleGeocodeBaseURL = "\(Constants.googleAPIBaseURL)maps/api/geocode/"
     var region: MKCoordinateRegion = MKCoordinateRegion(center: MapDetails.startingLocation, span: MapDetails.defaultSpan)
     
     init() {
-        if let region = LocationService.shared.mapRegion {
+        if let region = LocationService.shared.center {
             self.region = region
         }        
         addSubscription()
@@ -21,7 +22,7 @@ class MapsService {
     
     private func addSubscription() {
         Task {
-            for await value in LocationService.shared.$mapRegion.values {
+            for await value in LocationService.shared.$center.values {
                 await MainActor.run(body: {
                     if let value = value {
                         self.region = value
@@ -34,10 +35,9 @@ class MapsService {
     func getNearbySearchResults(query: String, radius: Int, type: GooglePlaceType? = nil) async throws -> GoogleNearbySearchModel? {
         do {
             let coordinate = region.center
-            let urlStr = "\(MapsService.googleMapsBaseURL)nearbysearch/json?keyword=\(query)&location=\(coordinate.latitude),\(coordinate.longitude)&radius=\(radius)\(type != nil ? "&type=\(String(describing: type!.rawValue))" : "")&key=\(Keys.googleAPIKey)"
+            let urlStr = "\(googleMapsBaseURL)nearbysearch/json?keyword=\(query)&location=\(coordinate.latitude),\(coordinate.longitude)&radius=\(radius)\(type != nil ? "&type=\(String(describing: type!.rawValue))" : "")&key=\(Keys.googleAPIKey)"
 
-            guard let url = URL(string: urlStr)
-            else { return nil }
+            guard let url = URL(string: urlStr) else { return nil }
             if let data = try? await NetworkingManager.get(url: url) {
                 var retVal = try JSONDecoder().decode(GoogleNearbySearchModel.self, from: data)
                 
@@ -65,10 +65,9 @@ class MapsService {
     func getAutoCompletePlaces(query: String, radius: Int, type: GooglePlaceType? = nil) async throws -> GoogleAutoCompleteModel? {
         do {
             let coordinate = region.center
-            let urlStr = "\(MapsService.googleMapsBaseURL)autocomplete/json?input=\(query)&location=\(coordinate.latitude),\(coordinate.longitude)&radius=\(radius)\(type != nil ? "&type=\(String(describing: type?.rawValue))" : "")&key=\(Keys.googleAPIKey)"
+            let urlStr = "\(googleMapsBaseURL)autocomplete/json?input=\(query)&location=\(coordinate.latitude),\(coordinate.longitude)&radius=\(radius)\(type != nil ? "&type=\(String(describing: type?.rawValue))" : "")&key=\(Keys.googleAPIKey)"
             
-            guard let url = URL(string: urlStr)
-            else { return nil }
+            guard let url = URL(string: urlStr) else { return nil }
             if let data = try? await NetworkingManager.get(url: url) {
                 return try JSONDecoder().decode(GoogleAutoCompleteModel.self, from: data)
             } else {
@@ -79,5 +78,19 @@ class MapsService {
         }
     }
     
-    
+    func getLocation(latitude: Double, longitude: Double) async throws -> ReverseGeocodeModel? {
+        do {
+            let urlStr = "\(googleGeocodeBaseURL)json?latlng=\(latitude),\(longitude)&key=\(Keys.googleAPIKey)"
+            
+            guard let url = URL(string: urlStr) else { return nil }
+            
+            if let data = try? await NetworkingManager.get(url: url) {
+                return try JSONDecoder().decode(ReverseGeocodeModel.self, from: data)
+            } else {
+                return nil
+            }
+        } catch {
+            throw error
+        }
+    }
 }
