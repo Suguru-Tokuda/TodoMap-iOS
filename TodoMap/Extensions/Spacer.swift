efa93c5130974fb62f418c91fdde97e0bb625118
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-09-27 14:46:50 -0500

    Added onTapGesture

--- TodoMap/Extensions/Spacer.swift
+++ TodoMap/Extensions/Spacer.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Spacer {
+    public func onTapGesture(count: Int = 1, perform action: @escaping () -> Void) -> some View {
+        ZStack {
+            Color.black.opacity(0.001).onTapGesture(count: count, perform: action)
+            self
+        }
+    }
+}
