bca18fe29cfbdd5afd879edafa8a591bebf923fb
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-09-27 14:47:39 -0500

    Added UITableView function calls.

--- TodoMap/MapTodoApp.swift
+++ TodoMap/MapTodoApp.swift
@@ -10,6 +10,13 @@ import SwiftUI
 @main
 struct MapTodoApp: App {
     let persistentContainer = PersistentController.shared
+    
+    init() {
+        UITableView.appearance().separatorStyle = .none
+        UITableView.appearance().backgroundColor = UIColor.clear
+        UITableViewCell.appearance().backgroundColor = UIColor.clear
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
